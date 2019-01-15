c57c68b1ccb5d32c1738e12df0ee6e5a973eb959
PXLcat <pixellecat@gmail.com>
2019-01-15 17:47:06 +0100

    1501

---
+++ IsoMap/Content/décor neige tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="décor neige tiles" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="Users/cehrhart/Pictures/décor neige tiles.png" width="300" height="300"/>
+ <terraintypes>
+  <terrain name="Pond" tile="21"/>
+ </terraintypes>
+ <tile id="2" terrain="0,0,,"/>
+ <tile id="11" terrain="0,,0,"/>
+ <tile id="20" terrain=",,0,0"/>
+ <tile id="21" terrain="0,0,0,0"/>
+ <tile id="29" terrain=",0,,0"/>
+ <tile id="38" terrain=",,,0"/>
+ <tile id="47" terrain="0,,,"/>
+ <tile id="56" terrain=",0,,"/>
+ <tile id="65" terrain=",,0,"/>
+</tileset>
---
+++ IsoMap/Content/herbe hiver.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="herbe hiver" tilewidth="32" tileheight="16" tilecount="72" columns="6">
+ <image source="Users/cehrhart/Pictures/herbe hiver.png" width="200" height="200"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="3" terrain="0,0,0,"/>
+ <tile id="4" terrain="0,0,,0"/>
+ <tile id="6" terrain=",0,,0"/>
+ <tile id="7" terrain="0,0,0,0"/>
+ <tile id="8" terrain="0,,0,"/>
+ <tile id="9" terrain="0,,0,0"/>
+ <tile id="10" terrain=",0,0,0"/>
+ <tile id="12" terrain=",0,,"/>
+ <tile id="13" terrain="0,0,,"/>
+ <tile id="14" terrain="0,,,"/>
+</tileset>
