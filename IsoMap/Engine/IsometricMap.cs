adc5094d35cc27c96523cc39205afb43d148e91e
PXLcat <pixellecat@gmail.com>
2019-01-17 18:10:53 +0100

    tentative dessin des calques

--- IsoMap/Engine/IsometricMap.cs
+++ IsoMap/Engine/IsometricMap.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TiledSharp;
 using Microsoft.Xna.Framework.Content;
+using System.Diagnostics;
 
 namespace IsoMap.Engine
 {
@@ -17,6 +18,11 @@ namespace IsoMap.Engine
 
         Point originTileCoord; //à utiliser pour les Tilesets 32x16
         Point originBlockCoord; //à utiliser pour les Tilesets 32x32
+        int tileOrBlockWidth;
+        Point tileSize;
+        Point blockSize;
+
+        // TODO : faire une fonction pour déterminer si un tileset a des tiles "plates" ou "block" et utiliser pour savoir quoi charger dans le Laod
 
         public void Load(ContentManager contentManager)
         {
@@ -24,6 +30,10 @@ namespace IsoMap.Engine
             tilesetsTextures = new Dictionary<string, Texture2D>();
             tilesetsTextures.Add("grassTileset", contentManager.Load<Texture2D>(snowMap.Tilesets[0].Name));//se référer à l'ordre dans le xml
             tilesetsTextures.Add("decorNeigeTileset", contentManager.Load<Texture2D>(snowMap.Tilesets[1].Name));//TODO générer par Factory
+
+            tileOrBlockWidth = snowMap.Tilesets[0].TileWidth;
+            tileSize = new Point(snowMap.Tilesets[0].TileWidth, snowMap.Tilesets[0].TileHeight);
+            blockSize = new Point(snowMap.Tilesets[1].TileWidth, snowMap.Tilesets[1].TileHeight);
 
             //originTileCoord = new Point(snowMap.Tilesets[0].TileWidth * (snowMap.Width / 4) - snowMap.Tilesets[0].TileWidth / 2, 0);
             originTileCoord = new Point(160, 0); //à suppr
@@ -46,9 +56,6 @@ namespace IsoMap.Engine
                 for (int y = 0; y < snowMap.Layers[i].Tiles.Count; y++) // Pour chaque tile
                 {
 
-
-
-
                     if (snowMap.Layers[i].Tiles[y].Gid != 0)
                     {
                         if (snowMap.Layers[i].Tiles[y].Gid < snowMap.Tilesets[1].FirstGid) //Correspond au 1er tileset //TODO généraliser les noms de variable et nombres de layer
@@ -63,13 +70,25 @@ namespace IsoMap.Engine
                                 , (int)Math.Floor((double)(snowMap.Layers[i].Tiles[y].Gid/ snowMap.Tilesets[0].Columns.Value) * snowMap.Tilesets[0].TileHeight)
                                 , snowMap.Tilesets[0].TileWidth
                                 , snowMap.Tilesets[0].TileHeight)
-                                , Color.White);
-
+                                , Color.White, 0f, Vector2.Zero, SpriteEffects.None, 0f);
                         }
                         // else if (tile.Gid < snowMap.Tilesets[x].FirstGid) si il y a d'autres layers
                         else
-                        {
-
+                        { //Attention Tileset "blocs"
+                            spriteBatch.Draw(tilesetsTextures.Values.ElementAt(1) //attention redondance, y'a que cet arg qui change
+                                                                                  //destinationRectangle :
+                                , new Rectangle(new Point(originTileCoord.X + (snowMap.Tilesets[1].TileWidth / 2) * orthogonalX - (orthogonalY * (snowMap.Tilesets[1].TileWidth / 2))
+                                    , originTileCoord.Y + ((snowMap.Tilesets[1].TileHeight / 2) * orthogonalX) + (orthogonalY * (snowMap.Tilesets[1].TileHeight / 2)))
+                                , new Point(snowMap.Tilesets[1].TileWidth, snowMap.Tilesets[1].TileHeight))
+                                //sourceRectangle :
+                                /*
+                                , new Rectangle((snowMap.Layers[i].Tiles[y].Gid- snowMap.Tilesets[1].FirstGid - 1) % snowMap.Tilesets[1].Columns.Value * snowMap.Tilesets[1].TileWidth
+                                , (int)Math.Floor((double)(snowMap.Layers[i].Tiles[y].Gid - snowMap.Tilesets[1].FirstGid / snowMap.Tilesets[0].Columns.Value) * snowMap.Tilesets[1].TileHeight)
+                                , snowMap.Tilesets[1].TileWidth
+                                , snowMap.Tilesets[1].TileHeight)
+                                */
+                                , new Rectangle(226, 0, 32, 32)
+                                , Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
                         }
                     }
                     orthogonalX++;
@@ -77,6 +96,10 @@ namespace IsoMap.Engine
                     {
                         orthogonalX = 0;
                         orthogonalY++;
+                        if (orthogonalY>snowMap.Height)
+                        {
+                            Debug.Write("fin de la map");
+                        }
 
                     }
                 }
