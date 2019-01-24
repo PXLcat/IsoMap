5ee0a8f68dffeaa5b111a26cf1c4841e2d5aaae1
PXLcat <pixellecat@gmail.com>
2019-01-24 17:35:49 +0100

    ajout des classes pour character

---
+++ IsoMap/Engine/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoMap.Engine
+{
+    public class Player
+    {
+        private static Player instance;
+        private Character currentCharacter;
+
+        public static Player Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Player();
+
+                }
+                return instance;
+            }
+        }
+        private Player() { }
+
+        public void Load(MainGame mG)
+        {
+            Factory factory = Factory.Instance;
+ 
+        }
+
+
+    }
+
+}
